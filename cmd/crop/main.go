f5c8d2cd67b7b89cd1923efe82ce31e1c11ba276
Michael Henderson <mdhender@mdhender.com>
2025-08-07 10:36:38 -0600

    implementd crop command to test hexg package
    Adds a new command-line tool 'crop' for cropping Worldographer .wxx files.
    The tool reads an input .wxx file, crops the map by a fixed amount (currently 14 tiles from each edge),
    and writes the cropped map to an output .wxx file.
    
    Updates the version to 0.19.0-alpha.

---
+++ cmd/crop/main.go
@@ -0,0 +1,114 @@
+// Copyright (c) 2025 Michael D Henderson. All rights reserved.
+
+// Package crop implements a command to crop a Worldographer file
+// by reading the XML from the input and writing it out the new file.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/maloquacious/wxx"
+	"github.com/maloquacious/wxx/models"
+	"github.com/maloquacious/wxx/xmlio"
+)
+
+func main() {
+	var showBuildInfo, showVersion, quiet bool
+	var inputFile, outputFile, debugUtf8XmlFile string
+
+	// TODO: Future sprint - add flag to let user specify the data version in the copied file
+	flag.BoolVar(&showVersion, "version", false, "show version")
+	flag.BoolVar(&showBuildInfo, "build-info", false, "show version with build info")
+	flag.StringVar(&inputFile, "input", "", "input .wxx file (required)")
+	flag.StringVar(&outputFile, "output", "", "output .wxx file (required)")
+	flag.StringVar(&debugUtf8XmlFile, "debug-utf8", "", "write debug UTF-8 XML file alongside compressed UTF-16 .wxx file")
+	flag.BoolVar(&quiet, "quiet", false, "suppress output messages")
+	flag.Parse()
+
+	if showVersion {
+		fmt.Printf("%s\n", wxx.Version().Short())
+		return
+	} else if showBuildInfo {
+		fmt.Printf("%s\n", wxx.Version().String())
+		return
+	}
+
+	if inputFile == "" || outputFile == "" {
+		fmt.Fprintf(os.Stderr, "usage: %s [options]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  -input file        input .wxx file (required)\n")
+		fmt.Fprintf(os.Stderr, "  -output file       output .wxx file (required)\n")
+		fmt.Fprintf(os.Stderr, "  -debug-utf8        write debug UTF-8 XML file alongside compressed UTF-16 .wxx file\n")
+		fmt.Fprintf(os.Stderr, "  -quiet             suppress output messages\n")
+		fmt.Fprintf(os.Stderr, "  -version           show version\n")
+		fmt.Fprintf(os.Stderr, "  -build-info        show version with build info\n")
+		os.Exit(2)
+	}
+
+	if !strings.HasSuffix(inputFile, ".wxx") {
+		fmt.Fprintf(os.Stderr, "error: input file must end with .wxx\n")
+		os.Exit(2)
+	}
+
+	if !strings.HasSuffix(outputFile, ".wxx") {
+		fmt.Fprintf(os.Stderr, "error: output file must end with .wxx\n")
+		os.Exit(2)
+	}
+
+	// Read the input file
+	input, err := xmlio.ReadFile(inputFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error reading %s: %v\n", inputFile, err)
+		os.Exit(1)
+	}
+
+	if !quiet {
+		fmt.Printf("input: %s (data version %s)\n", inputFile, input.MetaData.DataVersion.String())
+	}
+
+	if input.Tiles.TilesWide < 8 || input.Tiles.TilesHigh < 8 {
+		fmt.Printf("error: map too small to crop (want at least 8x8, got %dx%d)\n", input.Tiles.TilesWide, input.Tiles.TilesHigh)
+	}
+
+	fmt.Printf("input: %4d x %4d\n", input.Tiles.TilesWide, input.Tiles.TilesHigh)
+
+	// crop the map by subtracting 2 from each edge
+	lrow, rrow := 14, input.Tiles.TilesWide-14
+	if lrow < 0 || rrow < 0 {
+		fmt.Printf("error: map too small to crop (got %dx%d)\n", input.Tiles.TilesWide, input.Tiles.TilesHigh)
+	}
+	tcol, bcol := 14, input.Tiles.TilesHigh-14
+	if tcol < 0 || bcol < 0 {
+		fmt.Printf("error: map too small to crop (got %dx%d)\n", input.Tiles.TilesWide, input.Tiles.TilesHigh)
+	}
+	dstWidth, dstHeight := rrow-lrow+1, bcol-tcol+1
+	fmt.Printf(" crop: %4d x %4d\n", dstWidth, dstHeight)
+	// allocate cells for the destination map
+	dst := make([][]*models.Tile_t, dstWidth)
+	for i := range dst {
+		dst[i] = make([]*models.Tile_t, dstHeight)
+	}
+	// copy tiles within the crop area
+	for x := lrow; x <= rrow; x++ {
+		for y := tcol; y <= bcol; y++ {
+			dst[x-lrow][y-tcol] = input.Tiles.TileRows[x][y]
+		}
+	}
+	// update the input
+	input.Tiles.TilesWide, input.Tiles.TilesHigh = dstWidth, dstHeight
+	input.Tiles.TileRows = dst
+	fmt.Printf("input: %4d x %4d\n", input.Tiles.TilesWide, input.Tiles.TilesHigh)
+
+	// Write to the output file
+	err = xmlio.WriteFile(outputFile, input.MetaData.DataVersion, input, debugUtf8XmlFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error writing %s: %v\n", outputFile, err)
+		os.Exit(1)
+	}
+
+	if !quiet {
+		fmt.Printf("copied %s to %s\n", inputFile, outputFile)
+	}
+}
